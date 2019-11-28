cba5b5851e6bfb77986d56952a0a5adba8011306
CodePurble <ramprakash.chandrashekar@gmail.com>
2019-11-28 18:05:42 +0530

    updates

--- sanj.py
+++ sanj.py
@@ -2,5 +2,18 @@ inStr = input()
 
 inStr = inStr.split(' ')
 
-n = int(inStr[0])
-m = int(inStr[1])
+C = int(inStr[0])
+N = int(inStr[1])
+
+cost = []
+sell = []
+
+i = N;
+
+while(i > 0):
+    prices = input()
+    prices = prices.split(' ')
+    cost.append(prices[0])
+    sell.append(prices[1])
+    i -= 1
+
